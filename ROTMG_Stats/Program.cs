13536136d1fc46d90ccae9ae6bd8914f20e69314
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-12-15 22:31:01 +0100

    [Functions] New project for eZ shit [ROTMG_Stats] Now works from Functions

--- ROTMG_Stats/Program.cs
+++ ROTMG_Stats/Program.cs
@@ -1,34 +1,14 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
+using Functions;
 
 namespace rotmgstats
 {
     class Program
     {
-        public static void CW(string message) // Console Write
-        {
-            Console.WriteLine(message);
-        }
-
-        public static void CW(string message, object agrs)
-        {
-            Console.WriteLine(message, agrs);
-        }
-
-        public static void CW(int message)
-        {
-            Console.WriteLine(message);
-        }
-
-        public static void Sleep(int time) // Sleepy....
-        {
-            Thread.Sleep(time);
-        }
-
         static void Main(string[] args)
         {
-            CW("Hello");
+            Methods.CW("Hello");
             Console.Beep();
             Console.SetWindowSize(width: 65, height: 40);
 
@@ -38,8 +18,8 @@ namespace rotmgstats
                 int hp = 130;
 
                 hp = hp + rand.Next(25, 31);
-                Sleep(10);
-                CW(hp);
+                Methods.Sleep(10);
+                Methods.CW(hp);
 
                 using (var fw = new StreamWriter(path: "rotmgstats_hp.output"))
                 {
@@ -48,9 +28,9 @@ namespace rotmgstats
                 // Seems like this needs to be fixed lol http://gyazo.com/9a0b9fd6de02f576aff442b1dd095696.png
             }
 
-            Sleep(2000); // End of ROTMG_Stats application.
-            CW("");
-            CW("Press 'ESC' to stop the application.");
+            Methods.Sleep(2000); // End of ROTMG_Stats application.
+            Methods.CW("");
+            Methods.CW("Press 'ESC' to stop the application.");
             Console.ReadLine();
         }
     }
