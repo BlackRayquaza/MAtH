91294fe751738e9945f79f10f1b89617d165b324
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-12-15 22:22:57 +0100

    [ROTMG_Stats] Implemented Methods and some other stuff

--- ROTMG_Stats/Program.cs
+++ ROTMG_Stats/Program.cs
@@ -1,32 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace rotmgstats
 {
     class Program
     {
+        public static void CW(string message) // Console Write
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void CW(string message, object agrs)
+        {
+            Console.WriteLine(message, agrs);
+        }
+
+        public static void CW(int message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Sleep(int time) // Sleepy....
+        {
+            Thread.Sleep(time);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi!");
+            CW("Hello");
+            Console.Beep();
+            Console.SetWindowSize(width: 65, height: 40);
 
             for (int i = 0; i < 21; i++)
             {
                 Random rand = new Random();
                 int hp = 130;
+
                 hp = hp + rand.Next(25, 31);
-                Console.WriteLine(hp);
+                Sleep(10);
+                CW(hp);
 
                 using (var fw = new StreamWriter(path: "rotmgstats_hp.output"))
                 {
                     fw.WriteLine("[{0}] {1}", System.DateTime.Now, hp);
                 }
+                // Seems like this needs to be fixed lol http://gyazo.com/9a0b9fd6de02f576aff442b1dd095696.png
             }
-            // Seems like this needs to be fixed lol http://gyazo.com/9a0b9fd6de02f576aff442b1dd095696
-            Console.WriteLine("Press 'ESC' to stop the application.");
+
+            Sleep(2000); // End of ROTMG_Stats application.
+            CW("");
+            CW("Press 'ESC' to stop the application.");
             Console.ReadLine();
         }
     }
