538b18bf61570ad09274d13fa24973c4aa5068bc
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-12-15 21:21:16 +0100

    [ROTMG_Stats] Level up stats thingy

---
+++ ROTMG_Stats/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rotmgstats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+
+            for (int i = 0; i < 21; i++)
+            {
+                Random rand = new Random();
+                int hp = 130;
+                hp = hp + rand.Next(25, 31);
+                Console.WriteLine(hp);
+            }
+            // Seems like this needs to be fixed lol http://gyazo.com/9a0b9fd6de02f576aff442b1dd095696
+            Console.WriteLine("Press 'ESC' to stop the application.");
+            Console.ReadLine();
+        }
+    }
+}
