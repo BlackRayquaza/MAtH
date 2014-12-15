5c8ee54d43cf3d68ffaa3dc628a8b60f950731df
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-12-15 22:39:44 +0100

    Renamed stuff

--- Functions/Properties/AssemblyInfo.cs
+++ Functions/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Methods")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Functions")]
+[assembly: AssemblyDescription("eZ Functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Methods")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Open Realms")]
+[assembly: AssemblyProduct("Functions")]
+[assembly: AssemblyCopyright("Copyright © Lithium, Open Realms 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
