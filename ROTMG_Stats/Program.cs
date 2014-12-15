514a69602267b42407648c2604b322a55bb06fbe
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-12-15 21:35:33 +0100

    [ROTMG_Stats] Stuff now outputs to files

--- ROTMG_Stats/Program.cs
+++ ROTMG_Stats/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,11 @@ namespace rotmgstats
                 int hp = 130;
                 hp = hp + rand.Next(25, 31);
                 Console.WriteLine(hp);
+
+                using (var fw = new StreamWriter(path: "rotmgstats_hp.output"))
+                {
+                    fw.WriteLine("[{0}] {1}", System.DateTime.Now, hp);
+                }
             }
             // Seems like this needs to be fixed lol http://gyazo.com/9a0b9fd6de02f576aff442b1dd095696
             Console.WriteLine("Press 'ESC' to stop the application.");
