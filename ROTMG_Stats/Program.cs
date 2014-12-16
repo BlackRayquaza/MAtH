2d973f7b7cf8a4841f35edaafe52f32726bf3f0c
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-12-16 17:49:04 +0100

    [ROTMG_Stats] Yes.

--- ROTMG_Stats/Program.cs
+++ ROTMG_Stats/Program.cs
@@ -11,6 +11,7 @@ namespace rotmgstats
             string CurrentProgram;
             bool DebugMode = true;
             
+            FConsole.Print("What program do you wanna run?");
             CurrentProgram = Console.ReadLine().ToLower();
 
             if ((CurrentProgram == "" && DebugMode) || (CurrentProgram == "hp"))
@@ -19,6 +20,8 @@ namespace rotmgstats
                 Console.Beep();
 
                 Random rand = new Random();
+                
+                FConsole.Print("How much HP?");
                 int hp = int.Parse(Console.ReadLine());
 
                 if (hp == 130)
@@ -29,8 +32,12 @@ namespace rotmgstats
                         FConsole.Print(hp);
                     }
                 } else  {
+
+                    FConsole.Print("Min?");
                     int min = int.Parse(Console.ReadLine());
+                    FConsole.Print("Max?");
                     int max = int.Parse(Console.ReadLine());
+
                     for (int i = 0; i < 21; i++)
                     {
                         hp += rand.Next(min, max);
