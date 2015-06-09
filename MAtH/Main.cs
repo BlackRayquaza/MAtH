4f96ad02b20278389095ba107acf1c23c3f4f7f3
BlackRayquaza <shadowrealmrotmg@gmail.com>
2015-06-09 18:23:49 +0200

    New project
    :cat:

---
+++ MAtH/Main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MAtH
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
